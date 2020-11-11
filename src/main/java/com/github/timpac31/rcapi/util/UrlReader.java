1e400a938d150d48dc794e87279505547857fd47
timpac31 <timpac31@gmail.com>
2020-11-11 10:57:55 +0900

    set timeout

--- src/main/java/com/github/timpac31/rcapi/util/UrlReader.java
+++ src/main/java/com/github/timpac31/rcapi/util/UrlReader.java
@@ -4,18 +4,26 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.charset.Charset;
 
 public class UrlReader {
-	public static String readFromUrl(String url) throws MalformedURLException, IOException {
-		InputStream is = new URL(url).openStream();
+	public static String readFromUrl(String path) throws MalformedURLException, IOException {
+		URL url = new URL(path);
+		HttpURLConnection huc = (HttpURLConnection) url.openConnection();
+		huc.setConnectTimeout(2000);
+		huc.setReadTimeout(2500);
+		
+		BufferedReader br = null;
+		InputStream is = null;
 		try {
-			BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));		
+			is = huc.getInputStream();
+			br = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));		
 			StringBuilder sb = new StringBuilder();
 			int cp;
-			while ((cp = rd.read()) != -1) {
+			while ((cp = br.read()) != -1) {
 			  sb.append((char) cp);
 			}
 			return sb.toString();
@@ -23,5 +31,4 @@ public class UrlReader {
 			is.close();
 		}
 	}
-	
 }
